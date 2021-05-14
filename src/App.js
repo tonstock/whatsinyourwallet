c9479db781b60fbbdc6f2e4380a2f0e90d6bca3d
Spencer Stockton <spencerstockton@gmail.com>
2021-05-14 01:26:31 -0400

    Additional styling and added please search message on load

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 // import { json } from './testDB.js'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { Container, Avatar } from '@material-ui/core';
+import { Container, Avatar, Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import CryptoItem from './CryptoItem';
 import { makeStyles } from '@material-ui/core/styles';
@@ -12,7 +12,7 @@ function App() {
 
   const useStyles = makeStyles((theme) => ({
     mainInput: {
-      width: '40ch',
+      width: '45ch',
     },
     submitButton: {
       size: 'small',
@@ -23,6 +23,9 @@ function App() {
       width: theme.spacing(30),
       height: theme.spacing(30),
       align: 'center'
+    },
+    tokenListHeader: {
+      marginTop: '25px',
     }
   }));
 
@@ -60,21 +63,35 @@ function App() {
 
   const classes = useStyles();
 
+  let tokensElements = []
+  if (!tokens.length) {
+    tokensElements = (
+      <Grid xs={8} item>
+        <p>Please Search a Valid Eth Address</p>
+      </Grid>
+    )
+  } else {
+    tokensElements = tokens.map((token, index) => {
+      return <CryptoItem key={index} token={token}/>
+    })
+  }
+
   return (
     <div className="App">
         <Container align='center'>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
+            <Grid container spacing={2} justify='center'>
+                <Grid item xs={8}>
                     <Avatar alt="Remy Sharp" className={classes.samjavatar} src='https://consequence.net/wp-content/uploads/2017/03/samuel-jackson-headshot_crop.jpg'/>
                 </Grid>
-                <Grid item xs={12}>
+                <Grid item xs={8}>
                     <TextField id="standard" size='small' className={classes.mainInput} label="address" 
                       variant="outlined" onChange={handleChange}/>
                     <Button variant="outlined" size='large' className={classes.submitButton} onClick={handleSubmit}>Submit</Button>
                 </Grid>
-                {tokens.map((token, index) => {
-                    return <CryptoItem key={index} token={token}/>
-                })}
+                <Grid item xs={8}>
+                    <Typography variant='h6' className={classes.tokenListHeader}>Token List:</Typography>
+                </Grid>
+                    {tokensElements}
             </Grid>
         </Container>
     </div>
--- src/CryptoItem.js
+++ src/CryptoItem.js
@@ -1,12 +1,12 @@
 import {Typography, CardMedia, CardContent, Card} from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 
- function CryptoItem({token}) {
-
+function CryptoItem({token}) {
+  
   const {name, balance} = token;
 
   return (
-      <Grid xs={12} item>
+      <Grid xs={8} item>
         <Card>
           <CardMedia
             image='https://source.unsplash.com/random'
